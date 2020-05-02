ee19aa1fa92fb368d3eb1d5e52d85380c8d423ad
mayank raghuvanshi <mayank1272@hotmail.com>
2020-05-02 23:24:57 +0530

    env added

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT | 3000;
 const mongoose = require('mongoose');
-const config = require('./config.js');
+require('dotenv').config()
 //initialize parsers
 app.use(express.json());
 app.use(express.urlencoded({
     extended : true
 }))
 //initialize mongodb
-mongoose.connect(config.MONGO_URL)
+mongoose.connect(process.env.URL)
 .then(success=>console.log('MongoDB is connected.'))
 .catch(err=>console.error(err));
 
